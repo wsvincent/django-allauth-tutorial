b145d1d435cd7c7408e6496a79e6e51171c7e8c9
William Vincent <will@wsvincent.com>
2017-12-17 13:28:29 -0500

    update to Django 2.0

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.Home.as_view(), name='home'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', views.Home.as_view(), name='home'),
 ]
